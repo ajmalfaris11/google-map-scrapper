3412383aebf5be673ae88cb77580563be3720ec6
ajmal faris <ajmalfaris.me@gmail.com>
2026-07-05 10:07:00 +0530

    Add src/core/logger/Logger.ts

---
+++ src/core/logger/Logger.ts
@@ -0,0 +1,35 @@
+import pino from 'pino';
+import { env } from '../../config/env.validator';
+import fs from 'fs';
+import path from 'path';
+
+const logDir = path.join(process.cwd(), 'logs');
+
+// Ensure log directory exists
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir, { recursive: true });
+}
+
+export const logger = pino({
+  level: env.LOG_LEVEL,
+}, pino.transport({
+  targets: [
+    {
+      target: 'pino-pretty', // Log to stdout with pretty print
+      options: {
+        colorize: true,
+      },
+      level: env.LOG_LEVEL
+    },
+    {
+      target: 'pino-roll',
+      options: {
+        file: path.join(logDir, 'scraper'),
+        frequency: 'daily',
+        extension: '.log',
+        mkdir: true,
+      },
+      level: env.LOG_LEVEL
+    }
+  ]
+}));
