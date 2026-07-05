ddc9c22c6ba850e634db9a2cb82e2eee555d3fbd
ajmal faris <ajmalfaris.me@gmail.com>
2026-07-05 11:48:00 +0530

    Add or update src/providers/google-maps/selectors/BusinessSelectors.ts

--- src/providers/google-maps/selectors/BusinessSelectors.ts
+++ src/providers/google-maps/selectors/BusinessSelectors.ts
@@ -1,1 +1,14 @@
-export const BusinessSelectors = {};
+export const BusinessSelectors = {
+  businessCardLink: 'a[href*="/maps/place/"]',
+  endOfListMarker: 'span:has-text("You\'ve reached the end of the list")',
+  
+  // Extraction Selectors
+  name: 'h1.DUwDvf',
+  address: 'button[data-item-id="address"]',
+  website: 'a[data-item-id="authority"]',
+  phone: 'button[data-item-id^="phone:tel:"]',
+  rating: 'div.F7kvS span[aria-hidden="true"]',
+  reviews: 'span[aria-label*="reviews"]',
+  category: 'button.DkEaL',
+  hours: 'div[aria-label^="Hours"]' // or table containing hours
+};
