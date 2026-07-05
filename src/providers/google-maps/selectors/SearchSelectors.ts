639c704f4473f0792baa158299ee4c6eceda71cb
ajmal faris <ajmalfaris.me@gmail.com>
2026-07-05 11:37:00 +0530

    Add or update src/providers/google-maps/selectors/SearchSelectors.ts

---
+++ src/providers/google-maps/selectors/SearchSelectors.ts
@@ -0,0 +1,7 @@
+export const SearchSelectors = {
+  searchBox: 'input#searchboxinput',
+  searchButton: 'button#searchbox-searchbutton',
+  resultFeed: 'div[role="feed"]',
+  noResultsContent: '.Q2vNVc, .D29CYb',
+  captchaFrame: 'iframe[src*="recaptcha"]'
+};
