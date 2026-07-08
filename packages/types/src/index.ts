9d4aca10b948be7db7733527cc79967f0134b1df
ajmal faris <ajmalfaris.me@gmail.com>
2026-07-08 12:00:00 +0530

    Update packages/types/src/index.ts

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -3,3 +3,5 @@ export * from './interfaces';
 export * from './models/Business';
 export * from './models/ExtractionJob';
 export * from './models/Job';
+export * from './dto/JobOptions';
+export * from './dto/CreateJobDto';
