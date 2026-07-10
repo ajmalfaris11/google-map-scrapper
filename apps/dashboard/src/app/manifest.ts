fb44c4bc79ce38119bf74e3cc2c4592fa9cc12cb
ajmal faris <ajmalfaris.me@gmail.com>
2026-07-10 22:13:14 +0530

    Update manifest to point to app-icon.png

--- apps/dashboard/src/app/manifest.ts
+++ apps/dashboard/src/app/manifest.ts
@@ -11,7 +11,7 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#0052ff',
     icons: [
       {
-        src: '/icon.png',
+        src: '/app-icon.png',
         sizes: '512x512',
         type: 'image/png',
       },
